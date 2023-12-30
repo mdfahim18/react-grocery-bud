768bbfaee5c4e82f3ee2036d4f6a54d8af5d8d4e
mdfahim18 <mahmudulaminfahim@gmail.com>
2023-12-30 23:13:47 +0600

    title is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Alert from './components/Alert';
 import List from './components/List';
 
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 
 type PropsType = {
